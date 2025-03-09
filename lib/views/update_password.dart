4b7d105da6f6a91f6f1d9dab3b3d7b43a01eea80
Rukhsar <rukhsarnadeem842@gmail.com>
2025-03-09 06:48:57 +0500

    added UpdatePassword file

---
+++ lib/views/update_password.dart
@@ -0,0 +1,29 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:play_iq/views/check_email.dart';
+import 'package:play_iq/views/signup_screen.dart';
+import 'package:play_iq/views/successfully.dart';
+import 'package:play_iq/views/update_password.dart';
+
+import 'app/routes.dart';
+
+void main() {
+  runApp(DevicePreview(builder: (context) => const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Splash Screen',
+      theme: ThemeData.dark(),
+      home: UpdatePassword(),
+      // initialRoute: Routes.splash,
+      // getPages: AppRoutes.pages,
+    );
+  }
+}
